4e9df9295b7f7ee1c0669677bd899b1d89ae2c00
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-20 20:57:44 +0100

    solve truthy and falsy

--- js-conditions-and-booleans/challenges/truthy-and-falsy/js/index.js
+++ js-conditions-and-booleans/challenges/truthy-and-falsy/js/index.js
@@ -1,1 +1,16 @@
 //Write here your code.
+console.log ("The expression 2=='2' is", 2 == "2");
+console.log("The expression 2==='2' is", 2 === "2");
+console.log("The expression 2===2 is", 2 === 2);
+
+console.log("The expression 'hello world' is", "hello world");
+console.log("The expression '' is", '');
+console.log("The expression true is", true);
+
+console.log("The expression 0 is", 0);
+console.log("The expression -50 is", -50);
+console.log("The expression 50", 50);
+
+console.log("The expression 5 > 1 is", 5 > 1);
+console.log("The expression '1 > 10' is", "1 > 10");
+console.log("The expression 'false' is", "false");
