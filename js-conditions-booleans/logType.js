9d29023ea621ad4515732330f01a97aa563dceb3
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-21 14:00:47 +0100

    solve logType

--- js-conditions-booleans/logType.js
+++ js-conditions-booleans/logType.js
@@ -1,43 +1,45 @@
-let data;
-data = ["hello", 23, true];
+let data = [];
+// data = ["hello", 23, true];
 console.log (typeof data);
 
-switch (typeof data) {
-    case "":
-        console.log ("undefined!");
-        break;
-    case null:
-        console.log ("null!");
-        break;
-    case "string":
-        console.log ("string!");
-        break;
-    case "number":
-       if (Number.isNaN(data)) {
-         console.log ("not a number!");
-         break;
-       } else {
-        console.log (data, " is a number!") 
+function logType(data) {
+    switch (typeof data) {
+        case "undefined":
+            console.log ("undefined!");
+            break;
+        case "null":
+            console.log ("null!");
+            break;
+        case "string":
+            console.log ("string!");
+            break;
+        case "number":
+        if (Number.isNaN(data)) {
+            console.log ("not a number!");
+            } else {
+            console.log (data, " is a number!") 
+            }
+            break; 
+        case "object":
+        if (data === null) {
+            console.log (data, " null!");
+        } else if (Array.isArray (data)) {
+            console.log (data, " array!");
+        } else {
+            console.log (data, " object!");
         }
-        break; 
-    case "object":
-       if (data === null) {
-           console.log (data, " null!");
-       } else if (Array.isArray (data)) {
-           console.log (data, " array!");
-       } else {
-           console.log (data, " object!");
-       }
-       break;       
-    case true || false:
-        console.log ("boolean!");
-        break;
-    case "bigint":
-        console.log ("bigint!");
-        break;
-    case "function":
-        console.log ("function!");
-        break;    
-    default: 
-        console.log ("I have no idea!");
+        break;       
+        case "boolean":
+            console.log ("boolean!");
+            break;
+        case "bigint":
+            console.log ("bigint!");
+            break;
+        case "function":
+            console.log ("function!");
+            break;    
+        default: 
+            console.log ("I have no idea!");
+    }
 }
+
