df59a89ce691aa07275351678ed4724b5f141f08
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-21 11:47:03 +0100

    solve objects-challenge

--- js-objects-and-arrays_objects/index.js
+++ js-objects-and-arrays_objects/index.js
@@ -7,6 +7,13 @@ const personWithNameAgeEmail = {
   example: "example",
 };
 
+personWithNameAgeEmail.name = "Johnny Cash";
+personWithNameAgeEmail.age = 64;
+personWithNameAgeEmail.email = "ring@offire.com";
+delete personWithNameAgeEmail.example; 
+
+console.log(personWithNameAgeEmail);
+
 // EXERCISE 2
 // Set the values of name and age equal to the corresponding values of the personAlex object.
 
@@ -15,8 +22,10 @@ const personAlex = {
   age: 24,
 };
 
-const nameOfAlex = "Change me";
-const ageOfAlex = "Change me";
+const nameOfAlex = personAlex.name;
+const ageOfAlex = personAlex.age;
+
+console.log(nameOfAlex, ageOfAlex); 
 
 // EXERCISE 3
 // Change the value of name to "Alex" and the value of age to "35" by updating the object personToChange: person.existingProperty = newValue.
@@ -26,6 +35,11 @@ const personToChange = {
   age: 24,
 };
 
+personToChange.name = "Alex";
+personToChange.age = 35; 
+
+console.log(personToChange);
+
 // EXERCISE 4
 // Add the properties age "5" and breed "husky" to the object petPluto: pet.newProperty = newValue.
 
@@ -33,6 +47,11 @@ const petPluto = {
   name: "Pluto",
   species: "dog",
 };
+
+petPluto.age = 5;
+petPluto.breed = "husky";
+
+console.log (petPluto);
 
 export {
   personWithNameAgeEmail,
