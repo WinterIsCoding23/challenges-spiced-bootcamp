128f9946ebe3988c818ea2b3fc10aac21caff7fe
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-21 23:13:13 +0100

    work on task 1

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -9,18 +9,27 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 const output = document.querySelector('[data-js="output"]');
 
 pizzaInput1.addEventListener("input", () => {
-  // write your code here
+  pizzaSize1 = pizzaInput1.value;
 });
 
 pizzaInput2.addEventListener("input", () => {
-  // write your code here
+  pizzaSize2 = pizzaInput2.value;
 });
 
 // Task 1
 
 function calculatePizzaGain(diameter1, diameter2) {
-  // write your code here
+  const area1 = Math.PI * (diameter1/2) **2; 
+  const area2 = Math.PI * (diameter2/2) **2;
+  
+  const gainRelative = (area2 - area1) / area1 * 100;
+
+  let textContent = Math.round(gainRelative);  
+  
 }
+
+calculatePizzaGain (24, 24); 
+
 
 // Task 2
 
