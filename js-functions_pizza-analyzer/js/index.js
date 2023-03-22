36111d258afde7b02a0f2d4f64f2c4a9daa5a5dd
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-22 17:56:17 +0100

    solve pizza-analyzer

--- js-functions_pizza-analyzer/js/index.js
+++ js-functions_pizza-analyzer/js/index.js
@@ -8,34 +8,50 @@ const outputSection = document.querySelector('[data-js="output-section"]');
 
 const output = document.querySelector('[data-js="output"]');
 
+let pizzaSize1 = 24;
+let pizzaSize2 = 24;
+
 pizzaInput1.addEventListener("input", () => {
   pizzaSize1 = pizzaInput1.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza1, pizzaSize1);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
+
 
 pizzaInput2.addEventListener("input", () => {
   pizzaSize2 = pizzaInput2.value;
+  calculatePizzaGain(pizzaSize1, pizzaSize2);
+  updatePizzaDisplay(pizza2, pizzaSize2);
+  updateOutputColor(pizzaSize1, pizzaSize2);
 });
+
 
 // Task 1
 
 function calculatePizzaGain(diameter1, diameter2) {
-  const area1 = Math.PI * (diameter1/2) **2; 
-  const area2 = Math.PI * (diameter2/2) **2;  
-  const gainRelative = (area2 - area1) / area1 * 100;
-  output.textContent = Math.round(gainRelative);    
+  let area1 = Math.PI * (diameter1/2) **2; 
+  let area2 = Math.PI * (diameter2/2) **2;  
+  let gainRelative = (area2 - area1) / area1 * 100;
+  let textContent = Math.round(gainRelative);   
+  output.innerHTML = textContent; 
 }
-
-calculatePizzaGain (24, 20); 
 
 
 // Task 2
 
-function updatePizzaDisplay(pizzaElement, newSize) {
-  // write your code here
+function updatePizzaDisplay(pizzaElement, newSize) {  
+  let newDisplaySize = (newSize / 24) * 100;
+  pizzaElement.style.width = newDisplaySize + "px";
 }
+
 
 // Task 3
 
 function updateOutputColor(size1, size2) {
-  // write your code here
+  if (size1 >= size2) {
+    outputSection.style.backgroundColor = "var(--red)";
+  } else {
+    outputSection.style.backgroundColor = "var(--green)";
+  }
 }
