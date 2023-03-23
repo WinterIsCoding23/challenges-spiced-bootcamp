c34d1e3c78f34be8cb22f70a25eb36607f8f8157
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-23 15:17:36 +0100

    solve change-box-challenge

--- js-inputs-and-strings_change-box-appearance/js/index.js
+++ js-inputs-and-strings_change-box-appearance/js/index.js
@@ -2,8 +2,8 @@
 
 Change the appearance of the box according to the values of the sliders (color, border radius, rotation)
 
-– [ ] Add an event listener for each input element.
-– [ ] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
+– [x] Add an event listener for each input element.
+– [x] When the slider control of inputColor is moved, change the background color of the box. Hint: You need to work with the hsl() functional notation for colors.
 – [ ] When the slider control of inputRadius is moved, change the border radius of the box. If the control reaches the right end, make the box look like a circle.
 – [ ] When the slider control of inputRotation is moved, rotate the box. Hint: You may need to work with the CSS property "transform" and a value that contains "rotate" and "deg".
 
@@ -16,3 +16,27 @@ For further information check MDN:
 - about CSS declarations with "style": https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/style
 
 */
+
+const inputColor = document.querySelector(`[data-js = "input-color"]`);
+const inputRadius = document.querySelector(`[data-js = "input-radius"]`);
+const inputRotation = document.querySelector(`[data-js = "input-rotation"]`);
+
+const box = document.querySelector(`[data-js = "box"]`);
+
+inputColor.addEventListener ("input", (event) => {
+    const val = event.target.value; 
+    box.style.background = `hsl(${val}, 80%, 40%)`;
+    console.log(event.target.value);
+});
+
+inputRadius.addEventListener ("input", (event) => {
+    const val = event.target.value; 
+    box.style.borderRadius = `${val}%`;
+    console.log(event.target.value);
+});
+
+inputRotation.addEventListener ("input", (event) => {
+    const val = event.target.value; 
+    box.style.transform = `rotate(${val}deg)`;
+    box.style.width = `${val}px`;
+});
