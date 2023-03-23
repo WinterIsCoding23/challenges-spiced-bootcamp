2746b968ec403699de91ae1f568b6713a90e6456
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-23 17:10:17 +0100

    solve checkbox-input

--- js-forms_checkbox-input/js/index.js
+++ js-forms_checkbox-input/js/index.js
@@ -11,13 +11,32 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+
+hideTosError();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  const formElements = event.target.elements;
+  const formData = new FormData(event.target);
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  const tos = formElements.tos.checked;
+  console.log(tos);
 
   // --^-- write your code here --^--
+  if (tos === true) {
+    return hideTosError();
+  } else {
+    showTosError();
+  }
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
 });
+
+
+// Can you make the error message hidden initially (before the form is submitted)?
+
