99866e8a90964dc3221b2726714ccf5702fa0b24
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-24 23:29:58 +0100

    solve challenge

--- js-createelement_product-teaser/js/index.js
+++ js-createelement_product-teaser/js/index.js
@@ -9,3 +9,43 @@ const category3 = "Plankton Diet";
 const price = "149,99 €";
 const imageSrc =
   "https://unsplash.com/photos/3VOTHTrE614/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjU5NTM3NTA2&force=true&w=640";
+
+
+
+const secondTeaser = document.createElement("article");
+document.body.append(secondTeaser);
+    
+secondTeaser.innerHTML = `
+    <div class="product__body">
+    <div class="product__text-container">
+      <h2 class="product__name">${name}</h2>
+      <ul class="product__categories">
+        <li class="product__category">${category1}</li>
+        <li class="product__category">${category2}</li>
+        <li class="product__category">${category3}</li>
+      </ul>
+      <p class="product__description">
+        ${description}
+      </p>
+    </div>
+    <div class="product__image-container">
+      <img
+        class="product__image"
+        src=${imageSrc}
+        alt=""
+      />
+    </div>
+  </div>
+  <footer class="product__footer">
+    <span class="product__price">${price}</span>
+    <button type="button" class="product__buy-button" data-js="second-button">Buy</button>
+  </footer>
+`;
+
+secondTeaser.classList.add("product");
+
+const button = document.querySelector('[data-js="second-button"]');
+
+button.addEventListener ("click", () => {
+  console.log(name, price);
+})
