65374ee824d33d82c5cb905b7c9bb38c18e4427b
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-24 16:59:23 +0100

    solve challenge

--- js-forms-2_controlled-input-uppercase/js/index.js
+++ js-forms-2_controlled-input-uppercase/js/index.js
@@ -1,3 +1,11 @@
 console.clear();
 
 const input = document.querySelector('[data-js="toUpperCase"]');
+
+input.addEventListener("input", (event) => {
+    const afterEvent = event.target.value.toUpperCase ();
+    input.value = afterEvent; 
+})
+
+
+
