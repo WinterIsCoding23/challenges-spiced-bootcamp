f67756fd6afdbd22c794132514a5d31e2477179d
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-26 13:49:42 +0200

    solve challenge

--- js-forms-2_simple-live-calculator/js/index.js
+++ js-forms-2_simple-live-calculator/js/index.js
@@ -3,3 +3,13 @@ console.clear();
 const firstInput = document.querySelector('[data-js="firstValue"]');
 const secondInput = document.querySelector('[data-js="secondValue"]');
 const result = document.querySelector('[data-js="result"]');
+
+firstInput.addEventListener("input", ()=> {
+    result.textContent = firstInput.value * secondInput.value;
+    // console.log(firstInput.value, secondInput.value);
+})
+
+secondInput.addEventListener("input", () => {
+    result.textContent = firstInput.value * secondInput.value;
+    // console.log(firstInput.value, secondInput.value);
+})
