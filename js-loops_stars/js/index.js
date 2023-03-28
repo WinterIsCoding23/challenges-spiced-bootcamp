d9fda5fedfd17d1301638949beb90ab04926cd1c
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-28 23:09:40 +0200

    solve challenge

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -2,32 +2,28 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars(filledStars = 0) {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
-  for (let i=0; i<6; i++) {
-    const emptyStar = document.createElement("img");
-    //emptyStar.src = "../assets/star-empty.svg";
+  for (let i = 1; i < 6; i++) {
+    const imgElement = document.createElement("img");    
+    imgElement.setAttribute("src", "./assets/star-empty.svg");
 
-    if (filledStars < i) {
-      emptyStar.setAttribute("src", "./assets/star-empty.svg");
+    if (filledStars < i && filledStars > 0) {
+      imgElement.setAttribute ("src", "./assets/star-empty.svg"); 
     } else {
-      emptyStar.setAttribute("src", "./assets/star-filled.svg");
-    }  
-    emptyStar.addEventListener("click", () => {
-      console.log("clicked on a star");
-      render-statusbar(i);
-    })  
-    starContainer.append(emptyStar);
+      imgElement.setAttribute ("src", "./assets/star-filled.svg"); 
+    }
+    imgElement.addEventListener("click", () => {
+      console.log("it works!");
+      renderStars(i);
+    })
+    starContainer.append(imgElement); 
   }
 
-
-
-
-
-  //--^-- your code here --^--
+ //--^-- your code here --^--
 }
 
 renderStars();
