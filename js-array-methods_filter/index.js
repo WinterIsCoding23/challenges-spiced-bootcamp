00537b87d0848a8565fa3cc11e2f9047be296214
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-29 17:44:30 +0200

    solve challenge filter

--- js-array-methods_filter/index.js
+++ js-array-methods_filter/index.js
@@ -23,13 +23,28 @@ const cards = [
   },
 ];
 
-const onlyCardWithIdTwo = null;
+const onlyCardWithIdTwo = cards.filter((card) => {
+  return card.id="2";
+});
+console.log(onlyCardWithIdTwo);
 
-const allCardsWith3Tags = null;
+const allCardsWith3Tags = cards.filter((card) => {
+  return card.tags.length=3;
+});
+console.log(allCardsWith3Tags);
 
-const allCardsThatAreNotBookmarked = null;
+const allCardsThatAreNotBookmarked = cards.filter((card) => {
+  return card.isBookmarked===false;
+});
+console.log("allCardsThatAreNotBookmarked", allCardsThatAreNotBookmarked);
 
-const allCardsWithTagsHTMLOrJSThatAreBookmarked = null;
+const allCardsWithTagsHTMLOrJSThatAreBookmarked = cards.filter((card) => {
+  return (
+    (card.tags.includes("html") || card.tags.includes("JS")) &&
+    card.isBookmarked
+  );
+});
+console.log(allCardsWithTagsHTMLOrJSThatAreBookmarked);
 
 export {
   onlyCardWithIdTwo,
@@ -37,3 +52,4 @@ export {
   allCardsThatAreNotBookmarked,
   allCardsWithTagsHTMLOrJSThatAreBookmarked,
 };
+
