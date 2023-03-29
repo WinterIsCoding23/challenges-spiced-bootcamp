1921e5147e8e26a50770e582a4a425f7828f7bd1
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-29 11:24:50 +0200

    solve challenge 3

--- js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
+++ js-callback-functions_user-login-callback-3-with-multiple-arguments/js/index.js
@@ -5,3 +5,14 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+function showWelcomeMessage (userName, userRole) {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`)
+}
+
+// 1. 
+handleUserLogin(showWelcomeMessage);
+
+// 2. 
+handleUserLogin((userName, userRole) => {
+  console.log(`Welcome ${userName}! You are logged in now as ${userRole}.`)
+})
