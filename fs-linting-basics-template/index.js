b11a823363719c01eec506cb325e787bdc8d54d6
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-30 13:41:51 +0200

    solve challenge

--- fs-linting-basics-template/index.js
+++ fs-linting-basics-template/index.js
@@ -1,14 +1,16 @@
 function toggleButton() {
-  const cards = document.querySelectorAll("[data-js=card]");
-  const buttons = card.querySelectorAll("[data-js=buttons]");
-  cards.forEach(card => {
+  const cards = document.querySelectorAll('[data-js="card"]');
+  //const buttons = cards.querySelectorAll('[data-js="buttons"]');
+  cards.forEach((card) => {
         const button = card.querySelector('[data-js="button"]')
-        const link =  cards.querySelector('[data-js="link"]')
+        const link =  card.querySelector('[data-js="link"]')
         const arrow = card.querySelector('[data-js="arrow"]')
           button.addEventListener('click',() => {
-          link.classList.toggle("hide")
+          link.classList.toggle("hidden")
           arrow.classList.toggle("rotate")
           
     })
 })
 }
+
+toggleButton ();
