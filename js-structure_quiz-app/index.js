abefd91269fc248b4bbb28a4521eea58b28c7dd8
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-30 17:40:30 +0200

    clear index.js

--- js-structure_quiz-app/index.js
+++ js-structure_quiz-app/index.js
@@ -1,19 +1,10 @@
 // Store the cards in a global state
 import {cards} from "./utils/questions.js"; 
 import {CardList} from "./components/CardList/cardList.js";
+import {App} from "./components/App/app.js";
 
 
-function Header() {
-  const header = document.createElement("header");
-  header.classList.add("header");
-  header.innerHTML = /* html */ `
-	  <h1 class="header__title">Quiz-App</h1>
-	`;
-
-  return header;
-}
-
-function handleFormSubmit(event) {
+export function handleFormSubmit(event) {
   event.preventDefault();
 
   const formData = new FormData(event.target);
@@ -27,6 +18,7 @@ function handleFormSubmit(event) {
   };
 
   cards.unshift(newCard);
+  
 
   // To prevent the need to rerender, we add the new card to the DOM directly
   const cardList = document.querySelector(".card-list");
@@ -37,66 +29,6 @@ function handleFormSubmit(event) {
   event.target.elements.question.focus();
 }
 
-function Form() {
-  const form = document.createElement("form");
-  form.classList.add("form");
-  form.innerHTML = /* html */ `
-		<label for="question">Your question:</label>
-		<textarea
-			id="question"
-			rows="7"
-			name="question"
-			maxlength="150"
-			data-js="input-question"
-		></textarea>
-		<span class="form__character-count">
-			<span data-js="amount-left-question"></span> characters left
-		</span>
-		<label for="answer">Your answer:</label>
-		<textarea
-			id="answer"
-			rows="7"
-			name="answer"
-			maxlength="150"
-			data-js="input-answer"
-		></textarea>
-		<span class="form__character-count">
-			<span data-js="amount-left-answer"></span> characters left
-		</span>
-		<label for="tags">Tags:</label>
-		<input id="tags" type="text" name="tags" />
-		<button class="form__submit-button" type="submit">Submit</button>
-	`;
-
-  const formFields = form.querySelectorAll('[data-js*="input"]');
-  const counterOutputs = form.querySelectorAll("[data-js*=amount-left]");
-
-  formFields.forEach((formField, index) => {
-    counterOutputs.forEach((output) => {
-      output.innerText = formField.maxLength;
-    });
-    formField.addEventListener("input", () => {
-      const currentAmountLeft = formField.maxLength - formField.value.length;
-      counterOutputs[index].innerText = currentAmountLeft;
-    });
-  });
-
-  form.addEventListener("submit", handleFormSubmit);
-
-  return form;
-}
-
-
-
-
-
-function App() {
-  const app = document.createElement("main");
-  app.classList.add("app");
-  app.append(Header(), Form(), CardList());
-
-  return app;
-}
 
 // The render function renders the app to the DOM
 function render() {
