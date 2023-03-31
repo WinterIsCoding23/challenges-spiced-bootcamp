930f9a4e338f0035a4a9f103cdedb5d7a4ce3957
Markus Winter <markusdominicwinter@hotmail.com>
2023-03-31 15:04:04 +0200

    save first lines

--- js-async-functions_api-status/js/index.js
+++ js-async-functions_api-status/js/index.js
@@ -5,7 +5,7 @@ const apiStatusUrl = "https://example-apis.vercel.app/api/status";
 
 // Something needs to change here…
 // ↙️
-function checkIfApiIsAvailable() {
+async function checkIfApiIsAvailable() {
   /**
    * Hint 1:
    * Use the `fetch()` function and pass the `apiStatusUrl` into it
@@ -19,9 +19,42 @@ function checkIfApiIsAvailable() {
    * is okay and false if it is not.
    **/
   // --v-- write your code here --v--
+  try {
+    // if so, the icon should display ✅
+    icon = "✅";
+  } catch (error) {
+    // if not, the icon should display ❌
+    icon = "❌";
+  }  
+  
+
   // --^-- write your code here --^--
 }
 
 button.addEventListener("click", () => {
+  console.log (button);
   checkIfApiIsAvailable();
 });
+
+
+/*
+## Task
+
+Look at the [`js/index.js`](./js/index.js) file: There is a function called `checkIfAPIIsAvailable`; inside of this function, the `response` variable is declared with the result of `fetch(api)`.
+
+The function is called everytime you click the button.
+
+Your task is to refactor `checkIfAPIIsAvailable` so that - depending on `response.ok` - the icon displays either ✅ or ❌.
+
+You can use the following hints as guideline:
+
+- `fetch()` returns a promise, so make sure to wait for the result.
+- Check whether 'response.ok' is truthy;
+  - if so, the icon should display ✅
+  - if not, the icon should display ❌
+- Use a `try...catch` block.
+
+## Bonus:
+
+Can you display a '⏳' icon while waiting for the response?
+*/
