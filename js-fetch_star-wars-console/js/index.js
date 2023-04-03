cf9b4113aacfc934c62bb0dae3cf1ae3e85cacbb
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-03 15:38:13 +0200

    solve challenge

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,26 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+    try {
+        const response = await fetch (url);
+        const data = await response.json();
+
+        if (response.ok) {
+            console.log(data);
+            // console.log(typeof data);
+            console.log(data.results);
+            console.log(typeof data.results);
+            console.log(data.results[0]);
+            console.log(data.results[2]);
+            // console.log(typeof data.results[2]);
+            console.log(data.results[2].eye_color);
+        } else {
+            console.log("Bad response :-/");
+        }
+    } catch (error) {
+        console.log ("An error occurred");
+    }
+}
 
 fetchData();
