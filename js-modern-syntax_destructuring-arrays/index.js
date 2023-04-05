8b41d2bf4a99467d72e0b74e8b7f1aac33eca8ae
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-05 11:32:21 +0200

    solve challenge

--- js-modern-syntax_destructuring-arrays/index.js
+++ js-modern-syntax_destructuring-arrays/index.js
@@ -7,11 +7,16 @@ console.clear();
 const item = ["Egg", 0.25, 12];
 
 // example: export const [value1, value2] = array;
+export const [name, price, quantity] = item; 
+console.log(name, price, quantity);
 
 // EXERCISE 2
 // Use array destructuring to extract the variables "firstNameOfChris", "lastNameOfChris" and "ageOfChris".
 
 const personChris = [12, "Chris", "Owen"];
+
+export const [ageOfChris, firstNameOfChris, lastNameOfChris] = personChris;
+console.log(ageOfChris, firstNameOfChris, lastNameOfChris);
 
 // EXERCISE 3
 // Use array destructuring to extract the variables "firstNameOfAlex" and "lastNameOfAlex".
@@ -19,11 +24,17 @@ const personChris = [12, "Chris", "Owen"];
 
 const personAlex = ["Alex", 12, "Main"];
 
+export const [firstNameOfAlex, , lastNameOfAlex] = personAlex; 
+console.log(firstNameOfAlex, lastNameOfAlex);
+
 // EXERCISE 4
 // Use array destructuring to extract the last name from the array
 // as variable called "lastName".
 
 const students = ["Christina", "Jon", "Alexandare"];
+
+export const [, , lastName] = students;
+console.log(lastName);
 
 // EXERCISE 5
 // Use array destructuring to extract all of the names from this nested array.
@@ -31,13 +42,21 @@ const students = ["Christina", "Jon", "Alexandare"];
 
 const nestedStudents = ["Chris", ["Ahmad", "Antigoni"], ["Toby", "Sam"]];
 
+const [student1, [student2, student3], [student4, student5] ] = nestedStudents;
+console.log(student1, student2, student3, student4, student5);
+//console.log([student1, [student2, student3], [student4, student5] ]);
+
+//const newStudent = student3 + "Welt";
+//console.log(newStudent);
+
 // EXERCISE 6
 // Spread the values of the values array into the function call of the add function.
 // Keep in mind that you need to export the variable 'result' to make the test work.
 
 const values = [1, 6, 7, 9, 12, 5, 4];
 
-const result = add(1); // Spread values inside this function call
+const result = add(...values); // Spread values inside this function call
+console.log(result);
 
 function add(...values) {
   return values.reduce(
