755a9593c3f3cc3b09146f50fa856fb5bc2b1928
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-05 15:20:48 +0200

    solve challenge

--- js-unit-testing_get-animal/index.js
+++ js-unit-testing_get-animal/index.js
@@ -1,1 +1,12 @@
-export function getAnimal() {}
+export function getAnimal(animal) {
+
+    if (animal.endsWith("s")) {
+        return `I like ${animal}!`;
+    } else if (animal) {
+        return `I totally love ${animal}s!`;
+    } else if (animal === undefined) {
+        return "I do not like animals at all!"; 
+    }
+}
+
+console.log(getAnimal("mantis"));
