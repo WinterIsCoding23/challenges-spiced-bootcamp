b19e67d30348954f20488f04b58dcfc896da9786
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-05 18:25:28 +0200

    solve challenge

--- js-unit-testing_people/index.js
+++ js-unit-testing_people/index.js
@@ -2,18 +2,41 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  return people.filter((person) => person.age === age);
+}
 
-export function getPeopleNamesOlderThan(people, age) {}
+export function getPeopleNamesOlderThan(people, age) {
+  const filteredPeople = people.filter((person) => person.age > age);
+  const result = filteredPeople.map((person) => `${person.firstName} ${person.lastName}`);
+  return result;
+}
 
-export function getPeopleByLastName(people, lastName) {}
+export function getPeopleByLastName(people, lastName) {
+  const filteredPeople = people.filter((person) => person.lastName === lastName);
+  return filteredPeople;
+}
 
-export function findPersonById(people, id) {}
+export function findPersonById(people, id) {
+  const filteredPeople = people.find((person) => person.id === id);
+  return filteredPeople;
+}
 
-export function isAnyoneOlderThan(people, age) {}
+export function isAnyoneOlderThan(people, age) {
+  const filteredPeople = people.some((person) => person.age > age);
+  return filteredPeople;
+}
 
-export function getFullNamesSortedByAge(people) {}
+export function getFullNamesSortedByAge(people) {
+  const filteredPeople = people.sort((person1, person2) => person1.age - person2.age);
+  const result = filteredPeople.map((person) => `${person.lastName}`);
+  return result;
+}
