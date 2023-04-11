3d2c279e2135297d6ce05a55b81abaa2c882cd98
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-11 15:54:12 +0200

    solve challenge

--- react-basics_attributes/src/App.js
+++ react-basics_attributes/src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return <Component />;
 }
+
+function Component () {
+  return <article className="article">
+    <h2 className="article__title">Look at my beautiful header</h2>
+    <label htmlFor="react-input"></label>
+    <input id="react-input"></input>
+    <a className="article__link" href="https://reactnative.dev/">This is a link</a>
+  </article>
+}
+
+
