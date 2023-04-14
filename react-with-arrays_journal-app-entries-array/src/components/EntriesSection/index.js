89fdb5c512350c25de723bbabe26afe75029ab4a
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-14 16:26:58 +0200

    solve journal-app

--- react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.js
+++ react-with-arrays_journal-app-entries-array/src/components/EntriesSection/index.js
@@ -5,6 +5,38 @@ import Tabs from "../Tabs";
 import Tab from "../Tab";
 import Badge from "../Badge";
 
+const entries = [
+  {
+    id: 1000,
+    date: "Feb 5, 2025",
+    motto: "We are in a state of chaos",
+    notes:
+      "Today I learned about React State. It was fun! I can't wait to learn more.",
+  },
+  {
+    id: 999,
+    date: "Feb 4, 2025",
+    motto: "Props, Props, Props",
+    notes:
+      "Today I learned about React Props. Mad props to everyone who understands this!",
+  },
+  {
+    id: 998,
+    date: "Feb 3, 2025",
+    motto: "How to nest components online fast",
+    notes:
+      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
+  },
+  {
+    id: 997,
+    date: "Feb 2, 2025",
+    motto: "I'm a React Developer",
+    notes: "My React-ion when I learned about React: 😍",
+  },
+];
+
+// { index !== entries.length-1 && <Divider /> }  --> special expression, "&&" is NOT the usual "and-operator"
+
 export default function EntriesSection() {
   return (
     <section className="entries-section">
@@ -17,6 +49,18 @@ export default function EntriesSection() {
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
+        
+        { entries.map(({ id, motto, notes, date }, index)=> {
+          console.log(index)
+          return (
+                  <> 
+                  <Entry key={id} motto={motto} notes={notes} date={date}/>
+                  { index !== entries.length-1 && <Divider /> }   
+                  </>
+                  )
+          })
+        }   
+             
         <Entry
           date="Feb 27, 2028"
           motto="Thats life in the city"
