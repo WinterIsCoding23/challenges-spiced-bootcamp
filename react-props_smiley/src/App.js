f1e3533c938ec1c14d97472153b9d4f24c118593
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-16 18:40:18 +0200

    solve challenge

--- react-props_smiley/src/App.js
+++ react-props_smiley/src/App.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return <h1>Replace me with your component!</h1>;
+function Smiley ({isHappy}) {             // if "component accepts a prop" --> the prop is put as a parameter in the function-component
+  return <p>{isHappy ? "😁" : "😪"}</p>
 }
+
+export default function App() {
+  return <Smiley isHappy = {false} />
+}
+
+
+/*
+## Task
+
+Look at the [`src/App.js`](./src/App.js) file: the `App` component returns a heading which wants to be replaced by a component. Your task is to write a `Smiley` component which renders a smiley depending on a prop called `isHappy`.
+
+You can use the following hints as guideline:
+
+- Write the `Smiley` component inside of the [`App.js`](src/App.js).
+  - It accepts a prop called `isHappy` (make sure to destructure it).
+  - It returns a semantic HTML element which renders a happy smiley if `isHappy` is `true` and a sad one if its value is `false`.
+- Inside of the return statement of your `App` component, replace the heading with your `Smiley` component and pass it the `isHappy` prop (– or don't, if you want to display the unhappy smiley).
+*/
