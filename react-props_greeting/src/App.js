1d14c652057d2c4adf435a2a75fad925722c39fd
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-16 10:20:22 +0200

    solve challenge

--- react-props_greeting/src/App.js
+++ react-props_greeting/src/App.js
@@ -1,5 +1,28 @@
 import "./styles.css";
 
-export default function App() {
-  return <h1>Replace me with your component!</h1>;
+function Greeting ({name}) {
+  // if (name === "Jan" || name === "Sven" || name === "Marcell") {
+  // return <h1>Hello, Coach!</h1>
+  // } else {return <h1>Hello, {name}!</h1>} 
+
+  const coach = ["Jan", "Sven", "Marcell"];
+  //const isCoach = name === "Jan" || name === "Sven" || name === "Marcell";
+  //return isCoach ? <h1>Hello, Coach!</h1> : <h1>Hello, {name}!</h1>;
+  
+  return coach.includes(name) ? <h1>Hello, Coach!</h1> : <h1>Hello, {name}!</h1>;
 }
+
+export default function App() {
+  return <Greeting name = "Meister" />;
+}
+
+
+/*
+
+- Write the `Greeting` component inside of the [`App.js`](src/App.js).
+  - It accepts a prop called `name` (make sure to destructure it).
+  - It returns an HTML element and uses the `name` prop to render "Hello, [name]!";
+- Inside of the return statement of your `App` component, replace the heading with your `Greeting` component and pass it a `name` prop with a value of your choice.
+
+Update your `Greeting` component to show "Hello, Coach!" if the `name` prop is equal to one of your coaches.
+*/
