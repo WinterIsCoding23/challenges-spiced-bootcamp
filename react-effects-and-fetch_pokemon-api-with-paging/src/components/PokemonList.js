9b19eb99ec8494aa755ceb04a99346e9d56e91ec
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-17 21:36:26 +0200

    solve pokemon api-paging

--- react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
+++ react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.js
@@ -2,38 +2,61 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
-  const [page, setPage] = useState(0);
+  //const [page, setPage] = useState(0);        // ...used for alternative solution (see below) 
+  const [url, setUrl] = useState(
+    "https://pokeapi.co/api/v2/pokemon?offset=0"
+  )
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          `https://pokeapi.co/api/v2/pokemon?offset=${page}`
+          url
         );
         const data = await response.json();
-        console.log(data.results);
-        setPokemon(data.results);
+        //console.log(response);
+        setPokemon(data);
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, [page]);
+  }, [url]);          // ...whenever the url changes, do "useEffect" (aka fetch) 
 
-  function handleClick (){
-    setPage(page+20);
+  // function handleNextClick (){
+  //   setPage(page+20);
+  // }
+
+  // function handlePreviousClick (){
+  //   setPage(page-20);
+  // }
+  // --> both functions work - but not ideal since less flexible (e.g. amount of 20 pokemon per page changes in future) 
+
+  const nextPage = () => {
+    setUrl(pokemon.next);
   }
+
+  const previousPage = () => {
+    setUrl(pokemon.previous);
+  }
+
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button" onClick={handleClick}>Next Page</button>
+      {pokemon?.previous && (<button type="button" onClick={previousPage}>Previous Page</button>)} 
+      
+      {pokemon?.next && (<button type="button" onClick={nextPage}>Next Page</button>)}  
       <ul>
-        {pokemon.map(({ name }) => (
+        {pokemon?.results?.map(({ name }) => (        // ?: if pokemon exists, go ahead, check if results exists, then do what being told
           <li key={name}>{name}</li>
         ))}
       </ul>
     </main>
   );
 }
+
+
+// line 47: alternative to "?": pokemon && pokemon.results && pokemon.results.map (...)
+// nexPage-button: also should receive the same logic for when reaching end of list
+// to display the API-response in the browser: console - XHR - response
