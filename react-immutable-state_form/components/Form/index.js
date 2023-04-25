97c3439335cafd4ff620e8ac7f792d2e4404b272
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-25 22:11:42 +0200

    solve task 1

--- react-immutable-state_form/components/Form/index.js
+++ react-immutable-state_form/components/Form/index.js
@@ -10,11 +10,33 @@ export default function Form() {
     },
   });
 
-  function handleNameChange(event) {}
+  function handleNameChange(event) {
+    //console.log(event.target.value);
+    setMountain({
+      ...mountain, name: event.target.value,
+    })
+  }
 
-  function handleAltitudeChange(event) {}
+  function handleAltitudeChange(event) {
+    //console.log(event.target.value);
+    setMountain({
+      ...mountain, 
+        values: {
+          ...mountain.values, 
+            altitude: event.target.value,
+        } 
+    })
+  }
 
-  function handleMountainRangeChange(event) {}
+  function handleMountainRangeChange(event) {
+    setMountain({
+      ...mountain, 
+        values: {
+          ...mountain.values, 
+          mountainRange: event.target.value,
+        }
+    })
+  }
 
   return (
     <StyledForm>
@@ -49,3 +71,16 @@ export default function Form() {
     </StyledForm>
   );
 }
+
+
+/*
+### Updating with Spread Syntax
+
+Write the logic for `handleNameChange`, `handleAltitudeChange`, and `handleMountainRangeChange`.
+
+- Check the structure of the `mountain` object in state. It contains two keys: the `name` as string and the `values` object.
+- Use `setMountain` to assign `event.target.value` to its corresponding key in the state. Make sure to create a copy of the `mountain` object when setting a new state.
+- Note that the `altitude` and `mountainRange` keys are not at the top level of the `mountain` object; looks like you need to make a copy of this second level as well before you assign any new value.
+
+> ✨ You should now be able to change the values of the input fields and see these changes in the output area at the bottom.
+*/
