7105d5e370052604c64a9a3fe2d6c1e9d660215f
Markus Winter <markusdominicwinter@hotmail.com>
2023-04-25 21:44:51 +0200

    finisch challenge car-race

--- react-immutable-state_car-race/components/CarRace/index.js
+++ react-immutable-state_car-race/components/CarRace/index.js
@@ -8,24 +8,44 @@ import {
   Track,
 } from "./CarRace.styled";
 import { initialCars, getRandomDistance } from "../../utils/utils";
+import { current } from "immer";
+
+//input:
+import { useImmer } from "use-immer";
 
 const finishLine = 200;
 
 export default function CarRace() {
-  const [cars, setCars] = useState(initialCars);
+  const [cars, updateCars] = useImmer(initialCars);
 
   function moveCar(clickedCar) {
     const coveredDistance = getRandomDistance();
     console.log("clickedCar", clickedCar);
     console.log("coveredDistance", coveredDistance);
+  //   setCars((current)=>{
+  //     return current.map((car)=>{
+  //       if(car.emoji === clickedCar.emoji){
+  //         return {...car, position: {...car.position, lastDistance: coveredDistance, x: car.position.x + coveredDistance,}};
+  //       }
+  //       return car;
+  //     })
+  //   })
+
+  updateCars((draft)=>{
+    const chosenCar = draft.find((car)=>car.emoji === clickedCar.emoji);
+    chosenCar.position.lastDistance = coveredDistance;
+    chosenCar.position.x += coveredDistance;
+  });
   }
+
+  //console.log(cars);
 
   const winner = cars.find((car) => car.position.x >= finishLine);
 
   return (
     <>
       {winner ? (
-        <Winner winner={winner} onRestart={() => setCars(initialCars)} />
+        <Winner winner={winner} onRestart={() => updateCars(initialCars)} />
       ) : (
         <AllCarRoutes finishLine={finishLine}>
           <DistanceHeadline>Last Distance</DistanceHeadline>
