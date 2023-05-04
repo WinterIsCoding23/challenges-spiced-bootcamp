01a71a0fab7b96148a6d3460ceb548407c97a48d
Markus Winter <markusdominicwinter@hotmail.com>
2023-05-04 10:22:10 +0200

    solve challenge

--- backend-basics_greeting/server.js
+++ backend-basics_greeting/server.js
@@ -1,3 +1,13 @@
 import { createServer } from "node:http";
 
-// export const server = …
+export const server = createServer ((request, response)=> {
+    response.end("Hello, Baldrian");
+})
+
+
+/*
+- Import the `createServer` method from the `http` module.
+- Call `createServer` and pass it a function with two parameters: `request` and `response`.
+- In the function body of the function passed to `createServer`, call `response.end()` and pass it the string "Hello, [your name]!".
+- Export the server as a named export.
+*/
